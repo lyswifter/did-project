c205712be4a8c08d509a46a496b5d5676def611c
lyswifter <liuyong@tianrukj.com>
2022-11-11 12:19:08 +0800

    add cover images for vc

---
+++ src/components/showvc.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="container">
+        <div id="bgview">
+            HHH
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+#bgview {
+    width: 600px;
+    height: 842px;
+    background-image: url(../assets/img/卡面@2x.png);
+    background-repeat: no-repeat;
+}
+</style>
